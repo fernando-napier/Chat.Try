4587e5f8a2b341cc9df98d33e50af6b97837eaf8
fernando-napier <fernandonapier@gmail.com>
2022-11-23 15:36:26 -0600

    Enhance User Search (#8)

--- Chat.Try/Accessors/UserDbAccessor.cs
+++ Chat.Try/Accessors/UserDbAccessor.cs
@@ -1,10 +1,12 @@
 ﻿using Chat.Try.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Chat.Try.Accessors
 {
     public interface IUserDbAccessor
     {
         public string GetIdByUser(string user);
+        public List<string> SearchForUsers(string userSearch, List<string> excludedUsers);
     }
 
 
@@ -21,6 +23,11 @@ namespace Chat.Try.Accessors
         public string GetIdByUser(string user)
         {
             return _context.Users.FirstOrDefault(x => x.UserName == user)?.Id;
+        }
+
+        public List<string> SearchForUsers(string userSearch, List<string> excludedUsers)
+        {
+            return _context.Users.AsNoTracking().Where(x => x.UserName.StartsWith(userSearch) && !excludedUsers.Contains(x.UserName)).Select(x => x.UserName).OrderBy(x => x).ToList();
         }
     }
 }
