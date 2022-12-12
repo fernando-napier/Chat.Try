3015c3f2ee79c0f135bff9b41de064182174faa0
fernando <fernando.napier@gmail.com>
2022-12-12 16:18:34 -0600

    add embedded videos

---
+++ Fennorad/Models/YoutubeVideo.cs
@@ -0,0 +1,9 @@
+﻿namespace Fennorad.Models
+{
+    public class YoutubeVideo
+    {
+        public string Thumbnail { get; set; }
+        public string Title { get; set; }
+        public string VideoId { get; set; }
+    }
+}
