17fe7ecf5f7cbf5da216ec3e5f38761c8cd28da7
fernando <fernando.napier@gmail.com>
2024-04-30 08:45:21 -0500

    initial commit

--- Fennorad.Models/Models/ClaudeMessage.cs
+++ Fennorad.Models/Models/ClaudeMessage.cs
@@ -5,5 +5,7 @@
         public bool IsFromClaude { get; set; }
         public string Message { get; set; }
         public DateTimeOffset CreatedOn { get; set; }
+        public string Base64Image { get; set; }
+        public string MediaType { get; set; }
     }
 }
